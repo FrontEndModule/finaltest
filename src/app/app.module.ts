0949ac6b4a215494e2f28225ace1e226006e4bb3
thanh <thanhdc.consulting@gmail.com>
2018-12-08 09:51:47 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AwesomelistComponent } from './awesomelist/awesomelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AwesomelistComponent
   ],
   imports: [
     BrowserModule,
